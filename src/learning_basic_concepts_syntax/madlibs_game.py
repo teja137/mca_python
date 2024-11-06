5a4d1c42388895e19414d58e65ef2d071e97c211
teja137 <ajetdream@yahoo.com>
2024-11-07 00:01:49 +0530

    madlibs game

---
+++ src/learning_basic_concepts_syntax/madlibs_game.py
@@ -0,0 +1,14 @@
+#madlibs game
+
+
+adjective1 = input("Enter an ajective (description): ")
+noun1 = input("Enter a noune (person,place,thing): ")
+adjective2 = input("Enter an ajective (description): ")
+verb1 = input("Enter a verb ending with 'ing': " )
+adjective3 = input("Enter an ajective (description): ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
+
