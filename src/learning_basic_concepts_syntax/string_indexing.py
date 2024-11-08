51146ce2a3d22361f75c05111707d0434e8891b6
teja137 <ajetdream@yahoo.com>
2024-11-08 08:46:00 +0530

    string_indexing

---
+++ src/learning_basic_concepts_syntax/string_indexing.py
@@ -0,0 +1,21 @@
+# accessing elements
+
+credit_number = "1234-5678-90123-4567"
+
+print(credit_number[0])
+
+print(credit_number[:4])
+
+print(credit_number[5:9])
+
+print(credit_number[5:])
+
+print(credit_number[-1])
+
+print(credit_number[::2])
+
+last_four_digits = credit_number[-4:]
+print(last_four_digits)
+
+reverse_string = credit_number[::-1]
+print(reverse_string)
